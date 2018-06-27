e7b4c8c53bf2d63c1d27d64e810bac7ccffdf90e
elvin <elvin@mochasanani.com>
2018-06-27 18:59:54 +0800

    assignment 3

---
+++ basics-assignment-3-start/src/app/displaydetails/displaydetails.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-displaydetails',
+  templateUrl: './displaydetails.component.html',
+  styleUrls: ['./displaydetails.component.css']
+})
+export class DisplaydetailsComponent implements OnInit {
+  showDetail = false;
+  clickDetails = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onDisplayDetail() {
+    this.showDetail = !this.showDetail;
+    this.clickDetails.push(new Date());
+  }
+
+  getColor(index) {
+    return index >= 5 ? "blue" : "";
+   }
+}
